99984c6964c0070904391672b04aefbcae74e565
CassioRPS <cassiops2015@gmail.com>
2025-09-18 11:24:03 -0400

    subindo deploy.php

---
+++ deploy.php
@@ -0,0 +1,15 @@
+<?php
+$secret = 'SEU_SEGREDO_AQUI';
+$payload = file_get_contents('php://input');
+$signature = 'sha1=' . hash_hmac('sha1', $payload, $secret);
+
+if (!hash_equals($signature, $_SERVER['HTTP_X_HUB_SIGNATURE'] ?? '')) {
+    http_response_code(403);
+    exit('Acesso negado');
+}
+
+$repo = '/home4/cass3268/git.softflare.com.br';
+
+exec("cd $repo && git pull origin main");
+
+file_put_contents("$repo/deploy.log", date('Y-m-d H:i:s') . " Deploy executado\n", FILE_APPEND);
